7da594669e65c556969de660a429fe2d46dad23a
Palawan Collective <growpalawan@gmail.com>
2026-04-16 09:30:09 +0800

    Refactor App component to use React Query and Tooltips

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { AppProvider } from "@/context/AppContext";
 import Layout from "@/components/Layout";
-import AdminDashboard from "@/components/AdminDashboard";
-import Index from "@/pages/Index";
-import About from "@/pages/About";
-import Specials from "@/pages/Specials";
+import Index from "./pages/Index.tsx";
+import About from "./pages/About.tsx";
+import Specials from "./pages/Specials.tsx";
+import AdminDashboard from "./components/AdminDashboard";
+import NotFound from "./pages/NotFound.tsx";
 
-function App() {
-  return (
-    <AppProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout><Index /></Layout>} />
-          <Route path="/about" element={<Layout><About /></Layout>} />
-          <Route path="/specials" element={<Layout><Specials /></Layout>} />
-          <Route path="/admin" element={<AdminDashboard />} />
-        </Routes>
-      </BrowserRouter>
-    </AppProvider>
-  );
-}
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Sonner />
+      <AppProvider>
+        <BrowserRouter>
+          <Routes>
+            {/* Customer Pages (with Header/Footer) */}
+            <Route path="/" element={<Layout><Index /></Layout>} />
+            <Route path="/about" element={<Layout><About /></Layout>} />
+            <Route path="/specials" element={<Layout><Specials /></Layout>} />
+            
+            {/* Admin Page (no Header/Footer) */}
+            <Route path="/admin" element={<AdminDashboard />} />
+            
+            {/* 404 */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AppProvider>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
 
 export default App;

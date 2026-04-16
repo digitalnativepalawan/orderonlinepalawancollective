44e1dcbce48450045fa43f4df1f12f2fea91c941
Palawan Collective <growpalawan@gmail.com>
2026-04-16 08:32:24 +0800

    Remove ThemeProvider and adjust component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AppProvider } from "@/context/AppContext";
-import { ThemeProvider } from "@/components/ThemeProvider";
 import { Toaster } from "@/components/ui/toaster";
 import Layout from "@/components/Layout";
 import AdminDashboard from "@/components/AdminDashboard";
@@ -14,24 +13,22 @@ import Checkout from "@/pages/Checkout";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <AppProvider>
-        <BrowserRouter>
-          <Routes>
-            {/* All public routes - wrapped in Layout (Header + Footer) */}
-            <Route path="/" element={<Layout><Index /></Layout>} />
-            <Route path="/about" element={<Layout><About /></Layout>} />
-            <Route path="/specials" element={<Layout><Specials /></Layout>} />
-            <Route path="/cart" element={<Layout><Cart /></Layout>} />
-            <Route path="/checkout" element={<Layout><Checkout /></Layout>} />
-            
-            {/* Admin route - NO Layout wrapper (Admin has its own header) */}
-            <Route path="/admin" element={<AdminDashboard />} />
-          </Routes>
-        </BrowserRouter>
-        <Toaster />
-      </AppProvider>
-    </ThemeProvider>
+    <AppProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* All public routes - wrapped in Layout (Header + Footer) */}
+          <Route path="/" element={<Layout><Index /></Layout>} />
+          <Route path="/about" element={<Layout><About /></Layout>} />
+          <Route path="/specials" element={<Layout><Specials /></Layout>} />
+          <Route path="/cart" element={<Layout><Cart /></Layout>} />
+          <Route path="/checkout" element={<Layout><Checkout /></Layout>} />
+          
+          {/* Admin route - NO Layout wrapper (Admin has its own header) */}
+          <Route path="/admin" element={<AdminDashboard />} />
+        </Routes>
+      </BrowserRouter>
+      <Toaster />
+    </AppProvider>
   );
 }
 

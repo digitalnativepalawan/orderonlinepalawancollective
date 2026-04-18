7f175c189fd59d67668bd48ca7d184baaf649fa7
Palawan Collective <growpalawan@gmail.com>
2026-04-19 01:10:49 +0800

    Refactor routing and remove unused components

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { AppProvider } from "@/context/AppContext";
 import Layout from "@/components/Layout";
 import Index from "./pages/Index.tsx";
+import Rooms from "./pages/Rooms.tsx";
 import About from "./pages/About.tsx";
-import Specials from "./pages/Specials.tsx";
-import AdminDashboard from "./components/AdminDashboard";
 import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
@@ -15,23 +13,17 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Sonner />
-      <AppProvider>
-        <BrowserRouter>
-          <Routes>
-            {/* Customer Pages (with Header/Footer) */}
-            <Route path="/" element={<Layout><Index /></Layout>} />
-            <Route path="/about" element={<Layout><About /></Layout>} />
-            <Route path="/specials" element={<Layout><Specials /></Layout>} />
-            
-            {/* Admin Page (no Header/Footer) */}
-            <Route path="/admin" element={<AdminDashboard />} />
-            
-            {/* 404 */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </AppProvider>
+      <Toaster />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Index />} />
+            <Route path="/rooms" element={<Rooms />} />
+            <Route path="/about" element={<About />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
